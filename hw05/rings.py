7f0fb4c663aa0c3f4bf7528c0988954e63a69c82
Ian.Katzman <iankat2011@yahoo.com>
2012-02-12 18:38:06 -0500

    hw05 done

--- hw05/rings.py
+++ hw05/rings.py
@@ -31,6 +31,11 @@ screen.fill(WHITE)
 ##  parameter
 #################################
 
+pygame.draw.circle(screen,BLUE,(230,150), 80, 15)
+pygame.draw.circle(screen,BLACK,(400,150), 80, 15)
+pygame.draw.circle(screen,RED,(570,150), 80, 15)
+pygame.draw.circle(screen,YELLOW,(320,240), 80, 15)
+pygame.draw.circle(screen,GREEN,(490,240), 80, 15)
 
 ## Loop
 clock = pygame.time.Clock()
